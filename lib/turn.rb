9537f20794192cfde979fb8e8d2db1d041ca2fe0
Brian Plantico <bplantico@gmail.com>
2019-03-16 13:38:03 -0600

    Fixes Turn class and Turn test files

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,16 +1,14 @@
-# TURN CLASS
-
 class Turn
-  attr_reader :guess,
-              :card
+  attr_reader :card,
+              :guess
 
   def initialize(guess, card)
     @guess = guess
-    @card = card
+    @card  = card
   end
 
   def correct?
-    if guess == card.answer
+    if @guess == @card.answer
       true
     else
       false
@@ -26,26 +24,3 @@ class Turn
   end
 
 end
-
-# INTERACTION PATTERN
-#
-# pry(main)> turn = Turn.new("Juneau", card)
-# #=> #<Turn:0x00007f99842f0998 @card=#<Card:0x00007f800e29f0c9 @question=""What is the capital of Alaska?", @answer="Juneau", @guess="Juneau">
-#
-# pry(main)> turn.card
-# #=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
-#
-# pry(main)> turn.guess
-# #=> "Juneau"
-#
-# pry(main)> turn.correct?
-# #=> true
-#
-# pry(main)> turn.feedback
-# #=> "Correct!"
-#
-# pry(main)> turn.correct?
-# => false
-#
-# pry(main)> turn.feedback
-# => "Incorrect."
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -6,59 +6,54 @@ require 'pry'
 
 class TurnTest < Minitest::Test
 
-  def test_turn_exists
+  def test_it_exists
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     assert_instance_of Turn, turn
   end
 
-
-  def test_Turn_class_object_includes_Card_class_object
+  def test_Turn_class_includes_card_object
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("guess", card)
+    turn = Turn.new("Juneau", card)
 
-    assert_equal card, turn.card
+    assert_same card, turn.card
   end
 
-
-  def test_turn_has_a_guess
+  def test_turn_includes_guess
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("guess", card)
+    turn = Turn.new("Juneau", card)
 
-    assert_equal "guess", turn.guess
+    assert_equal "Juneau", turn.guess
   end
 
-
-  def test_if_guess_is_correct?
+  def test_guess_is_correct
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
     assert_equal true, turn.correct?
   end
 
-
-  def test_feedback_if_guess_is_correct
+  def test_feedback_when_correct
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
-    assert_includes "Correct!", turn.feedback
+    assert_equal "Correct!", turn.feedback
   end
 
 
-  def test_if_guess_is_incorrect?
+  def test_guess_is_incorrect
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
 
     assert_equal false, turn.correct?
   end
 
-
-  def test_feedback_if_guess_is_incorrect
+  def test_feedback_when_incorrect
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
 
-    assert_includes "Incorrect.", turn.feedback
+    assert_equal "Incorrect.", turn.feedback
   end
 
 end
