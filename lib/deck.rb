005ec9b7ad3d393373674e06e30150a77bb40ea3
Brian Plantico <43261385+bplantico@users.noreply.github.com>
2019-10-30 10:09:35 -0600

    Update deck.rb

--- lib/deck.rb
+++ lib/deck.rb
@@ -16,28 +16,3 @@ class Deck
   end
 
 end
-
-
-# A Deck is initialized with an array of Card objects.
-# A Deck should also be able to return cards based on a given category.
-# The Deck class should respond to the following interaction pattern:
-#
-# pry(main)> cards = [card_1, card_2, card_3]
-#
-# pry(main)> deck = Deck.new(cards)
-# => #<Deck:0x00007fa160a38ed8...>
-#
-# pry(main)> deck.cards
-# #=> [#<Card:0x00007fa16104e160...>, #<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
-#
-# pry(main)> deck.count
-# #=> 3
-#
-# pry(main)> deck.cards_in_category(:STEM)
-# #=> [#<Card:0x00007fa160a62e90...>, #<Card:0x00007fa161a136f0...>]
-#
-# pry(main)> deck.cards_in_category(:Geography)
-# #=> [#<Card:0x00007fa16104e160...>]
-#
-# pry(main)> deck.cards_in_category("Pop Culture")
-# #=> []
